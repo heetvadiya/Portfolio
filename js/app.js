f4acde50966261d22872e2c7d490f58e9c6d96e9
heetvadiya <108529736+heetvadiya@users.noreply.github.com>
2023-09-02 22:15:46 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,20 @@ $(document).ready(function(){
     dotsClass:'dots'
     });
 });
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav=document.querySelector('.mobile-nav');
+
+
+hamberger.addEventListener('click',function(){
+    // jena pr apply karvanu chhe e means mobileeNav jema apda mobile nu navigation chhe e
+    // .classlist:  property which shows all class of mobileNav  
+    //add() function will add open class
+    mobileNav.classList.add('open');
+})
+
+times.addEventListener('click',function(){
+    //remove : do work as name
+    mobileNav.classList.remove('open');
+})
